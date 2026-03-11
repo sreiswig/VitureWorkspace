1a5367ed0af8fddb949ea12fdefb20d423d60ead
sreiswig <sam.reiswig@gmail.com>
2026-03-10 21:03:45 -0500

    setup simple flutter app

--- devenv.nix
+++ devenv.nix
@@ -10,6 +10,9 @@
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
 
+  android.enable = true;
+  android.flutter.enable = true;
+
   # https://devenv.sh/processes/
   # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
 
@@ -27,20 +30,11 @@
     git --version # Use packages
   '';
 
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
   # https://devenv.sh/tests/
   enterTest = ''
     echo "Running tests"
     git --version | grep --color=auto "${pkgs.git.version}"
   '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
 
   # See full reference at https://devenv.sh/reference/options/
 }
